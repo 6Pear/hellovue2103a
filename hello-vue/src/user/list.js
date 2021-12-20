539223725537753f7b2e1f3d3f7a688875dc7ee9
6Pear <1194669652@qq.com>
2021-12-20 20:17:18 +0800

    提交

---
+++ hello-vue/src/user/list.js
@@ -0,0 +1,13 @@
+const listForm = {
+    template:'\
+    <div>\
+    <h2>列表页</h2> \
+    <table border="1" cellspacing="0" cellpadding="0" >\
+    <tr><th>姓名</th><th>年龄</th><th>籍贯</th></tr>\
+    <tr><td>李白</td><td>18</td><td>北京</td></tr>\
+    <tr><td>杜甫</td><td>19</td><td>上海</td></tr>\
+    <tr><td>王维</td><td>20</td><td>深圳</td></tr>\
+    </table>\
+    </div>\
+    '
+}
